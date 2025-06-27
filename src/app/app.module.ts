1e93fd9ad229bd9b569eacae59dcb8bf07a1f7ce
bokargiu <erickjstaack07@gmail.com>
2025-06-27 10:17:15 -0300

    addUserPageComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,24 @@ import { CdkMenuModule } from '@angular/cdk/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserPageComponent } from './Pages/user-page/user-page.component';
+import { UserPageInterfaceComponent } from './Components/UserPageComponents/user-page-interface/user-page-interface.component';
+import { SideMenuComponent } from './Components/UserPageComponents/side-menu/side-menu.component';
+import { SearchAreaComponent } from './Components/UserPageComponents/search-area/search-area.component';
+import { LocalizeNearMapComponent } from './Components/UserPageComponents/localize-near-map/localize-near-map.component';
+import { ButtomAdBarComponent } from './Components/UserPageComponents/buttom-ad-bar/buttom-ad-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPageComponent,
+    UserPageInterfaceComponent,
+    SideMenuComponent,
+    SearchAreaComponent,
+    LocalizeNearMapComponent,
+    ButtomAdBarComponent,
+
   ],
   imports: [
     BrowserModule,
