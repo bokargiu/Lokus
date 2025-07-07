2084d58869aefd3f9645b89e43902f744b4ff6d2
thaís <tallmann.thais@gmail.com>
2025-07-07 12:00:56 -0300

    Inseri o card de slide imagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-import { CdkMenuModule } from '@angular/cdk/menu';
+import { CdkMenuModule } from '@angular/cdk/menu';ng 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,12 +12,12 @@ import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { FootingComponent } from './Components/GlobalComponents/footing/footing.component';
 import { HeadMenuComponent } from './Components/GlobalComponents/head-menu/head-menu.component';
 import { SearchPageComponent } from './Pages/search-page/search-page.component';
-import { ImagesComponent } from './Components/GlobalComponents/images/images.component';
 import { CardCompanyComponent } from './Components/GlobalComponents/card-company/card-company.component';
 import { CompaniesPageComponent } from './Pages/companies-page/companies-page.component';
 import { SignupCompanyPageComponent } from './Pages/signup-company-page/signup-company-page.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
 import { SignupClientPageComponent } from './Pages/signup-client-page/signup-client-page.component';
+import { SlidePicturesComponent } from './Components/GlobalComponents/slide-pictures/slide-pictures.component'; 
 
 
 @NgModule({
@@ -25,12 +25,12 @@ import { SignupClientPageComponent } from './Pages/signup-client-page/signup-cli
     AppComponent,
     HomePageComponent,
     SearchPageComponent,
-    ImagesComponent,
     CardCompanyComponent,
     CompaniesPageComponent,
     SignupCompanyPageComponent,
     LoginPageComponent,
-    SignupClientPageComponent
+    SignupClientPageComponent,
+    SlidePicturesComponent
   ],
   imports: [
     BrowserModule,
