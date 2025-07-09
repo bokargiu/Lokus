725be59d15436a70762aa0381486bb44e08a3d6d
cauetrainotti <trainotti.caue@gmail.com>
2025-07-09 11:40:19 -0300

    alteração nos cards e adição de icones

---
+++ src/app/Pages/coworkin-page/coworkin-page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+
+@Component({
+  selector: 'app-coworkin-page',
+  templateUrl: './coworkin-page.component.html',
+  styleUrls: ['./coworkin-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CoworkinPageComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { MatIconModule } from '@angular/material/icon'
 import { CdkMenuModule } from '@angular/cdk/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoworkinPageComponent } from './Pages/coworkin-page/coworkin-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoworkinPageComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     HttpClientModule,
     CdkMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
